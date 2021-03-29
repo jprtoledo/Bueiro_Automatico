7fb38eab3bb09666be11eecdcae538fc6237cc12
Juan Toledo <juan.prtoledo@gmail.com>
2021-03-28 21:02:48 -0300

    terceira

--- website/login/models.py
+++ website/login/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class datalog(models.Model):
+    device_name = models.CharField(max_length=250)
     is_active = models.CharField(max_length=50)
-    device_name = models.CharField(max_length=255)
     last_occurrence = models.DateTimeField(auto_now=True)
     objects = models.Manager()
 
